f50613e5f90e0921fa18327500862a100cf57bc0
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-08-08 10:37:27 -0500

    feat: :sparkles: agregar tokenInterceptor y index para envío automático de token

---
+++ red-social-angular/src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem(environment.tokenKey); // Ajusta si usas otro storage
+
+    if (token) {
+      const cloned = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
