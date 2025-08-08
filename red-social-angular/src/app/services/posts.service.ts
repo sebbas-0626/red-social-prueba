c4f45d383e2f4575b2bc05c4f696f56bfbe2fc2f
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-08-08 10:58:33 -0500

    feat: :sparkles: comentando los metodos del posts.service

--- red-social-angular/src/app/services/posts.service.ts
+++ red-social-angular/src/app/services/posts.service.ts
@@ -12,23 +12,30 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
+  // This method is used to get all posts
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.API_URL);
   }
 
+  // This method is used to get a single post by ID
   createPost(postData: CreatePostRequest): Observable<Post> {
     return this.http.post<Post>(this.API_URL, postData);
   }
+  // This method is used to update a post
+  deletePost(postId: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.API_URL}/${postId}`);
+  }
 
+  // This method is used to like a post
   likePost(postId: string): Observable<{ message: string }> {
     return this.http.post<{ message: string }>(`${this.API_URL}/${postId}/like`, {});
   }
-
+  // This method is used to unlike a post
   unlikePost(postId: string): Observable<{ message: string }> {
     return this.http.delete<{ message: string }>(`${this.API_URL}/${postId}/like`);
   }
-
-  deletePost(postId: string): Observable<{ message: string }> {
-    return this.http.delete<{ message: string }>(`${this.API_URL}/${postId}`);
+  //Get all posts from a specific user
+  getUserPosts(userId: number): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.API_URL}/user/${userId}`);
   }
 }
