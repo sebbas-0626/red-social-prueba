b56f8f5243a54d2f202831ece6b13deafc851a77
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-12-02 21:27:38 -0500

    fix: :bug: ajuste eliminacion de navbar import

--- red-social-angular/src/app/app.component.ts
+++ red-social-angular/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './features/auth/services/auth.service';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
+// import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
